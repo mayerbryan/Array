8b7640ad2bf5a1eeea3b9a1e124caca094f62ab2
Bryan Mayer <mayerbryan@mail.com>
2022-03-23 13:50:21 -0300

    option menu

--- Program.cs
+++ Program.cs
@@ -12,17 +12,26 @@ namespace Array
             string[] playersNames = new string[playersAmount];
 
             for(int index = 0; index < playersAmount; index++){
+                Console.Clear();
                 Console.WriteLine($"type the player name {index+1} ");
                 playersNames[index] = Console.ReadLine();
             }
 
-            Console.Clear()
-
-            Console.WriteLine("Players List");
-            foreach(var item in playersNames){
+            Console.WriteLine("Do you want to see the list?");
+            Console.WriteLine("1 for yes");
+            Console.WriteLine("2 to exit");
+            short option = short.Parse(Console.ReadLine());
+            switch (option){
+                case 1: 
+                Console.Clear();
+                Console.WriteLine("Players List");
+                foreach(var item in playersNames){
                 Console.WriteLine(item);
-            }
-
+                } break;
+                case 2: Environment.Exit(0); break;
+            }     
         }
+
+        
     }
 }
